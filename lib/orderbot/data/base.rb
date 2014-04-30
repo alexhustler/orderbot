a59f0d19ef1aea0e736598483112e9c5e11d2e06
Roman <roman@slatestudio.com>
2014-04-29 22:42:53 -0700

    Fix to_h Base method

--- lib/orderbot/data/base.rb
+++ lib/orderbot/data/base.rb
@@ -7,7 +7,7 @@ module OrderBot
     end
 
     def to_h
-      instance_values.collect{|name, value| [name, value.respond_to?(:to_h) ? value.to_h : value ]}.to_h.symbolize_keys
+      instance_values.collect{|name, value| [name, value.is_a?(Base) ? value.to_h : value ]}.to_h.symbolize_keys
     end
   end
 end
