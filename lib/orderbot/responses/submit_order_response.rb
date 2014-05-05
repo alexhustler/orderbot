da68662c9b57e5b89416deb73e199e3d41324f95
Roman <roman@slatestudio.com>
2014-05-05 14:32:37 -0700

    Fix success & order id getters

--- lib/orderbot/responses/submit_order_response.rb
+++ lib/orderbot/responses/submit_order_response.rb
@@ -1,13 +1,13 @@
 module OrderBot
   class SubmitOrderResponse < AbstractResponse
     def successful?
-      @data[:pl_submit_order_response][:pl_submit_order_result][:order_process_result][:is_success]
+      @data[:pl_submit_order_response][:pl_submit_order_result][:order_process_result][:pl_order_result][:is_success]
     rescue
       false
     end
 
     def order_id
-      @data[:pl_submit_order_response][:pl_submit_order_result][:order_process_result][:order_bot_order_id]
+      @data[:pl_submit_order_response][:pl_submit_order_result][:order_process_result][:pl_order_result][:order_bot_order_id]
     end
 
     def error_message
