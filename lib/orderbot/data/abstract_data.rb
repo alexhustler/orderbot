d30d295463193dd65b5216435f9b9bf7b20df032
Roman <roman@slatestudio.com>
2014-05-05 13:27:09 -0700

    Constantize -> Camalize

--- lib/orderbot/data/abstract_data.rb
+++ lib/orderbot/data/abstract_data.rb
@@ -7,7 +7,7 @@ module OrderBot
     end
 
     def to_h
-      instance_values.map{|key, value| [key.classify, value]}.to_h
+      instance_values.map{|key, value| [key.camelize, value]}.to_h
     end
   end
 end
