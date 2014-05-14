e1608ba652f1e4b61c01fb9e1db5f6a98111a926
Roman <roman@slatestudio.com>
2014-05-14 14:59:44 -0700

    Status fetch fix

--- lib/orderbot/responses/order_status_response.rb
+++ lib/orderbot/responses/order_status_response.rb
@@ -7,8 +7,8 @@ module OrderBot
     end
 
     def orders
-      @data[:pl_get_order_status_response][:pl_get_order_status_result][:orders][:pl_order_status_header].collect do |order_data|
-        OrderStatus.new(order_data)
+      @data[:pl_get_order_status_response][:pl_get_order_status_result][:orders].collect do |order_data|
+        OrderStatus.new(order_data[1])
       end
     end
 
