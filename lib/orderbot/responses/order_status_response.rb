2cb095d56b3047b5d4c8b49deb0703fc313bcb62
Roman <roman@slatestudio.com>
2014-05-14 21:30:24 -0700

    Correctly process both order status updates - hashes and arrays

--- lib/orderbot/responses/order_status_response.rb
+++ lib/orderbot/responses/order_status_response.rb
@@ -7,8 +7,11 @@ module OrderBot
     end
 
     def orders
-      @data[:pl_get_order_status_response][:pl_get_order_status_result][:orders].collect do |order_data|
-        OrderStatus.new(order_data[1])
+      order_data = @data[:pl_get_order_status_response][:pl_get_order_status_result][:orders][:pl_order_status_header]
+      if order_data.is_a?(Hash)
+        [ OrderStatus.new(order_data) ]
+      else
+        order_data.collect{|data| OrderStatus.new(data)}
       end
     end
 
